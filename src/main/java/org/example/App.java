7c6573b7fb21dc8e834aba753e5b89a1227e0c1f
Aina <7836112@alumnos.ufv.es>
2021-10-09 11:00:18 +0200

    Creada lectura fichero, pendiente crear class videoteca

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,6 +1,12 @@
 package org.example;
 
 
+import com.google.gson.Gson;
+
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Map;
 import java.util.Scanner;
 
 /**
@@ -37,7 +43,27 @@ public class App
     }
 
     public static void opcion1(){
-        
+        try {
+            // create Gson instance
+            Gson gson = new Gson();
+
+            // create a reader
+            Reader reader = Files.newBufferedReader(Paths.get("peliculas.json"));
+
+            // convert JSON file to map
+            Map<?, ?> map = gson.fromJson(reader, Map.class);
+
+            // print map entries
+            for (Map.Entry<?, ?> entry : map.entrySet()) {
+                System.out.println(entry.getKey() + "=" + entry.getValue());
+            }
+
+            // close reader
+            reader.close();
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 
     public static int menu(){
